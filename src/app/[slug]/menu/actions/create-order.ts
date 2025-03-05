7bd058a54d7619a379c8e27c1acfcbb0e3c9419b
LuksDi4s <luuks177@gmail.com>
2025-03-04 23:08:01 -0300

    feat: add costomer name and cpf in prisma and create order

---
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -0,0 +1,15 @@
+"use server";
+
+interface CreateOrderInput {
+  customerName: string;
+  costomerCpf: string;
+  products: Array<{
+    id: string;
+    price: number;
+    quantity: number;
+  }>;
+  consumptionMethod: string;
+  restaurantId: string;
+}
+
+export const createOrder = async (input: CreateOrderInput) => {};
